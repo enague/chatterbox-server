ff7af96bd5b73fb9e2cbb7d6e024fefcf7ba65d2
enague <ennague@gmail.com>
2018-05-15 17:10:53 -0700

    after refactoring and code clean-up

--- server/basic-server.js
+++ server/basic-server.js
@@ -1,35 +1,66 @@
 /* Import node's http module: */
 var http = require('http');
+var fs = require('fs');
+var handleRequest = require('./request-handler.js');
 
-
-// Every server needs to listen on a port with a unique number. The
-// standard port for HTTP servers is port 80, but that port is
-// normally already claimed by another server and/or not accessible
-// so we'll use a standard testing port like 3000, other common development
-// ports are 8080 and 1337.
 var port = 3000;
-
-// For now, since you're running this server on your local machine,
-// we'll have it listen on the IP address 127.0.0.1, which is a
-// special address that always refers to localhost.
 var ip = '127.0.0.1';
 
+var setup = {
+  '/classes/messages': function(request, response) {
+    handleRequest.requestHandler(request, response);
+  },
 
+  '/': function(request, response) {
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.write(fs.readFileSync('./client/index.html'));
+    response.end();
+  },
 
-// We use node's http module to create a server.
-//
-// The function we pass to http.createServer will be used to handle all
-// incoming requests.
-var handleRequest = require('./request-handler.js');
-//
-// After creating the server, we will tell it to listen on the given port and IP. */
+  '/styles/styles.css': function(request, response) {
+    response.writeHead(200, {'Content-Type': 'text/css'});
+    response.write(fs.readFileSync('./client/styles/styles.css'));
+    response.end();
+  },
+
+  '/scripts/app.js': function(request, response) {
+    response.writeHead(200, {'Content-Type': 'application/javascript'});
+    response.write(fs.readFileSync('./client/scripts/app.js'));
+    response.end();
+  },
+
+  '/scripts/dynamic.js': function(request, response) {
+    response.writeHead(200, {'Content-Type': 'application/javascript'});
+    response.write(fs.readFileSync('./client/scripts/dynamic.js'));
+    response.end();
+  },
+
+  '/bower_components/underscore/underscore.js': function(request, response) {
+    response.writeHead(200, {'Content-Type': 'application/javascript'});
+    response.write(fs.readFileSync('./client/bower_components/underscore/underscore-min.js'));
+    response.end();
+  },
+
+  '/bower_components/DOMPurify/dist/purify.min.js': function(request, response) {
+    response.writeHead(200, {'Content-Type': 'application/javascript'});
+    response.write(fs.readFileSync('./client/bower_components/DOMPurify/dist/purify.min.js'));
+    response.end();
+  },
+
+  '/bower_components/moment/min/moment.min.js': function(request, response) {
+    response.writeHead(200, {'Content-Type': 'application/javascript'});
+    response.write(fs.readFileSync('./client/bower_components/moment/min/moment.min.js'));
+    response.end();
+  }
+};
+
 var server = http.createServer(function(request, response) {
   console.log('Serving request type ' + request.method + ' for url ' + request.url);
-  if(request.url === '/classes/messages') {
-    handleRequest.requestHandler(request, response);
+
+  if (setup[request.url]) {
+    setup[request.url](request, response);
   } else {
-    statusCode = 404;
-    response.writeHead(statusCode, headers);
+    response.writeHead(404, handleRequest.headers);
     response.end();
   }
 
--- server/request-handler.js
+++ server/request-handler.js
@@ -1,5 +1,3 @@
-var fs = require('fs');
-
 var results = [];
 
 var headers = {
@@ -25,7 +23,7 @@ var actions = {
   'POST': function(request, response) {
     request.on('data', (chunk) => {
       results.push(JSON.parse(chunk.toString()));
-      sendData(response, 201, chunk);
+      sendData(response, 201, {chunk});
     });
   },
   'OPTIONS': function(request, response) {
@@ -39,27 +37,5 @@ var requestHandler = function(request, response) {
   }
 };
 
-
-  // if (request.url === '/') {
-  //   var html = fs.readFileSync('./client/index.html');
-    
-  //   response.writeHead(200, {'Content-Type': 'text/html'});
-  //   response.write(html);
-  //   response.end();
-  // } else if (request.url === '/styles/styles.css') {
-  //   var css = fs.readFileSync('./client/styles/styles.css');
-
-  //   response.writeHead(200, {'Content-Type': 'text/css'});
-  //   response.write(css);
-
-  //   response.end();
-  // } else if (request.url === '/bower_components/underscore/underscore.js ') {
-  //   var underscore = fs.readFileSync('./client/bower_components/underscore/underscore.js');
-
-  //   response.writeHead(200, {'Content-Type': 'application/javascript'});
-  //   response.write(underscore);
-  //   response.end();
-  // }
-
-
 module.exports.requestHandler = requestHandler;
+module.exports.headers = headers;
